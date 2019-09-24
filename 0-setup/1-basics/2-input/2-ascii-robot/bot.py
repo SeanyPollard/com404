197c75cd9178530f3732ac30b8b6760ac14a4a76
SeanyPollard <4polls65@solent.ac.uk>
2019-09-24 15:32:50 +0000

    robo bois eyes

---
+++ 0-setup/1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,12 @@
+print("Please enter a character for the eyes:")
+eye = input()
+print("""
+      ________
+     /  ____  \\
+    [  (""" + eye + "__" + eye + """)  ]
+     \\________/
+        /  \\
+       //  \\\\
+      / |  | \\
+      --|  |--
+        \\__/""")
