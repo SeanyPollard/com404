cee8fb7cb040832820ad421839b9e285852364a8
SeanyPollard <4polls65@solent.ac.uk>
2019-10-08 08:56:58 +0000

    5th while task

---
+++ 01-basics/04-repetition/01-while-loop/05-sum-100/bot.py
@@ -0,0 +1,14 @@
+#Initial statement
+print("Calculating the sum of the first 100 numbers...")
+
+#Init vars
+counter = 0
+total = 0
+
+#While to count through all numbers up to 100, adding as it goes
+while counter < 100:
+    counter += 1
+    total += counter
+
+#Completion statement
+print("... Done! the answer is " + str(total))
